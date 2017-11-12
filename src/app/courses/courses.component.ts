a6b7850c8eb23e72f2c9e294cd50a03d0ae7a5f3
sajib <sajib@appscode.com>
2017-11-12 10:52:37 +0600

    Add component, service

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { CoursesService } from '../courses.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent implements OnInit {
+export class CoursesComponent {
+  title = 'List of Courses';
+  courses;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    const service = new CoursesService();
+    this.courses = service.getCourses();
   }
+
 
 }
