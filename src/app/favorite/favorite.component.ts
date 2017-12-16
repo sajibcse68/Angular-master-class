75bd2005d799af843e882f3f7f1ec76f98007475
sajib <sajib@appscode.com>
2017-12-16 23:04:24 +0600

    Add view encapsulation (shadow DOM)

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css'],
+/*
   styles: [
 `
   .glyphicon {
@@ -11,6 +12,8 @@ import { Component, OnInit, Input } from '@angular/core';
 }
 `
   ]
+ */
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class FavoriteComponent implements OnInit {
   @Input() isFavorite: boolean;
